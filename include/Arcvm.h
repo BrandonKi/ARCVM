7226f4e4ec44b13be7a9cd863dcd756deda0529a
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2021-09-21 12:52:13 -0400

    add CLI arg parsing

---
+++ include/Arcvm.h
@@ -0,0 +1,41 @@
+#include <cstdint>
+#include <string>
+#include <vector>
+
+namespace arcvm {
+
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+
+struct File {
+    std::string file_name;
+    std::string file_data;
+    // accept anything convertible to string
+    File(std::string name) : file_name(name) {}
+    File(std::string_view name) : file_name(name) {}
+};
+
+enum class OptimizationLevel : i8 {
+    custom = -1,
+    zero = 0,
+    one = 1,
+    two = 2,
+    three = 3
+};
+
+enum class Mode : i8 { binary = 0, text = 1 };
+
+struct Args {
+    OptimizationLevel opt_level;
+    Mode mode;
+    std::vector<File> input_files;
+    std::string output_file_name;
+};
+
+} // namespace arcvm
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,56 @@
-#include <iostream>
+// CLI for calling ARCVM directly
 
-int main() {
-    std::cout << "Hi\n";
+#include "Arcvm.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace arcvm;
+
+Args get_args(int argc, char *argv[]) {
+    Args args;
+    // set defaults
+    args.opt_level = OptimizationLevel::zero;
+    args.mode = Mode::text;
+    args.output_file_name = "a.exe";
+
+    for(std::string_view string : std::vector<std::string_view>(argv + 1, argv + argc)) {
+        switch(string[0]) {
+            case '-':
+                switch(string[1]) {
+                    case 'O':
+                        errno = 0;
+                        args.opt_level = static_cast<OptimizationLevel>(std::strtol(string.substr(2).data(), nullptr, 10));
+                        if (errno != 0) {
+                            std::cerr << "Invalid argument given to '-O'\n";
+                            args.opt_level = OptimizationLevel::zero;
+                        }
+                        break;
+                    case 'o':
+                        args.output_file_name = string.substr(2);
+                        break;
+                    case 'b':
+                        args.mode = Mode::binary;
+                        break;
+                    case 't':
+                        args.mode = Mode::text;
+                        break;
+                }
+                break;
+            // if an argument doesn't start with '-' then assume it's an input file name
+            default:
+                args.input_files.emplace_back(string);
+        }
+    }
+    return args;
+}
+
+int main(int argc, char *argv[]) {
+    Args args = get_args(argc, argv);
+    std::cout << i32(args.opt_level) << '\n';
+    std::cout << i32(args.mode) << '\n';
+    std::cout << args.output_file_name << '\n';
+    for(auto& [name, data] : args.input_files)
+        std::cout << name << '\n';
 }

aceb3fb71e610cda96c7b584539f06c528c201b3
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2021-09-22 23:08:50 -0400

    started to finalize the IRGen API

--- include/IRGenerator.h
+++ include/IRGenerator.h
@@ -1,20 +1,63 @@
 
+enum class Instruction : i8 {
+    add,
+    index,
+    call,
+    ret,
+    alloc,
+    load,
+    store
+};
+
+enum class ValueType : i8 {
+    none,
+    pointer,
+    reference,
+    immediate
+};
+
+// FIXME does not work with float immediate values
+struct Value {
+    ValueType type;
+    i64 value;    
+};
+
+struct Entry {
+    i32 dest;
+    Instruction instruction;
+    std::vector<Value> arguments;
+};
+
+struct Block {
+    std::vector<Entry> entries;
+};
+
+struct Module {
+    std::vector<Block> blocks;
+};
+
+// TODO make generating code from multiple threads more friendly
 class IRGenerator {
   public:
     IRGenerator();
 
+    Module create_module();
+    void use_module();
+    void link_modules();
 
+    void gen_label();
+    Value gen_inst(Instruction, std::vector<Value>);
+    Value get_param(i32);
+    void gen_def();
+    void gen_decl();
+
+  private:
+    std::vector<Module> modules;
 };
 
 
 /*
-IRGenerator gen;
 
-gen.createModule(...);
-
-gen.useModule(...);
-
-std::vector<Module> mod_list = gen.getModules(...);
 
 
 */
